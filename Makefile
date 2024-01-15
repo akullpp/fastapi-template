81c2d7026d14a44e8be76df0de320d476d2f074f
Andreas Kull <akullpp@gmail.com>
2024-01-15 13:55:57 +0100

    Update

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ clean:
 	rm -rf .venv
 
 .PHONY: install
-install: requirements.in
+install: clean requirements.in
 	python3 -m venv .venv
 	chmod +x .venv/bin/activate
 	. .venv/bin/activate
--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from collections.abc import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
-from app import config
+from app.config import config
 
 engine = create_async_engine(
     f"postgresql+asyncpg://{config['DB_USER']}:{config['DB_PASS']}@{config['DB_URL']}/{config['DB_NAME']}",
@@ -11,6 +11,7 @@ engine = create_async_engine(
 )
 
 AsyncSessionFactory = async_sessionmaker(engine, autoflush=False, expire_on_commit=False)
+
 
 async def get_session() -> AsyncGenerator:
     async with AsyncSessionFactory() as session:
